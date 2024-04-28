c5441f9557e6541db2ccd0b86ee72094c09480a7
Ilya <ilya.yunkins@gmail.com>
2024-04-28 23:30:46 +0300

    Com

--- h1.cpp
+++ h1.cpp
@@ -3,56 +3,63 @@
 // H1. Read text from given text file. Find most frequent words in input file starting on each English alphabet letter. Print in another text file for each English alphabet letter the most frequent word. Word is string containing only English alphabet letters. All other symbols in text file are interpreted as word separators.
 
 
-// СКОЛЬКО РАЗ ВСТРЕЧАЕТСЯ КАЖДЫЙ СИМВОЛ //
-    // list<string> names = {"Apple","apple" "aardvark", "banana" ,"Bandana" ,"circus","Cirrus","Dog", "dogma" ,"effort", "Effort" ,"Gamma" ,"gamma" ,"hat", "Iota" ,"iota" ,"Joke" ,"joke","Kite", "kite" ,"Lima" ,"lima" ,"moma", "moma" ,"nectar" ,"Nectar", "Opera", "opera"};
-
-    // std::map<char , int> st;
-
-    // for (auto &n : names) {
-    //     for(char &c:n){
-    //         c = tolower(c);
-    //         st[c]++;
-    //     }
-
-    // }
-
-    // for (const auto &pair : st) {
-    //     std::cout << pair.first << ": " << pair.second << std::endl;
-    // }
-
 #include<iostream>
 #include <fstream>
 #include <list>
 #include <map>
 
-
-
 using namespace std;
 int main(){
     ifstream file("examples.txt");
-    string word;
-    list<char> alphabet;
     map<string,int> mf;
-    int num = 0;
-    string word1;
-
+    map<char,int>mf2;
+    map<char,string>mf3;
+    int maxValue = 0;
+    int maxValue1 = 0;
+    int count = 0;
+    string thatWord = "";
+    char prevLet='\0';
+    string word;
     if(file.is_open()){
         while(file>>word){
             for(char &c:word){
-                c=tolower(c);   
+                c=tolower(c);
             }
             mf[word]++;
         }
-        for (auto it=mf.begin(); it!=mf.end(); ++it)
-            cout << it->first << " => " << it->second << '\n';
-        }else{
-                cout<<"Error to open a file!";
-            }
-            file.close();
-}
 
-//найти сколько раз повторяются слова
-//найти самое частое для каждой букв
-//собрать в один список
-//отсортировать
-//присвоить значения мэпу с алфавитом  
+        for (char letter = 'a'; letter <= 'z'; ++letter) {
+            mf3[letter];
+        }
+        for(auto &v:mf){
+            if(prevLet!=v.first[0]){
+                maxValue = 0;
+            }
+            if(v.second>=maxValue){
+                thatWord = v.first;
+                maxValue=v.second;
+                prevLet = v.first[0];
+            }
+            mf2[prevLet]=maxValue;
+        }
+
+        for(auto &n:mf2){
+            for(auto &i:mf){
+                if(n.second == i.second && n.first == i.first[0]){
+                    mf3[n.first] = i.first;
+                }
+            }
+        }
+
+    }
+        
+        cout<<"\n";
+        for (auto it=mf3.begin(); it!=mf3.end(); ++it){
+            cout << it->first << " => " << it->second << '\n';
+         }
+
+        cout<<"\n";
+
+
+        file.close();
+        }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,7 @@
-    // std::sort(letters.begin(), letters.end());
+        // for (auto it=mf.begin(); it!=mf.end(); ++it){
+        //     cout << it->first << " => " << it->second << '\n';
+        // }
+        // cout<<"\n";
+        // for (auto it=mf2.begin(); it!=mf2.end(); ++it){
+        //     cout << it->first << " => " << it->second << '\n';
+        //  }
